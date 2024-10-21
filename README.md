7978c221e63fa34e7e07a45e31e3f34b7f417b49
altarzariol <alice.tarzariol@aau.at>
2024-10-21 21:05:06 +0200

    update

---
+++ README.md
@@ -0,0 +1,30 @@
+# A Declarative Approach to Tackle Sawmill Production Scheduling with Answer Set Programming
+
+Benchmark of Sawmill Production Scheduling, ASP encodings and experimental results.
+
+
+## Requirements
+
+* [Clingo](https://potassco.org/clingo/) 
+
+## Repeat experiments
+For n in [0..4]
+```
+$ ./run.sh n
+```
+
+
+## Authors
+
+Giacomo Da Col  
+Philipp Fleiss  
+Alice Tarzariol  
+Erich C. Teppan  
+Elena Wiegelmann
+
+
+## License
+
+This project is licensed under the MIT License 
+
+
--- run.sh
+++ run.sh
@@ -11,7 +11,6 @@ for ins in ./ins$INDIR/*.lp
         do
             filename=$(basename -- "$ins")
 		    id="${filename%.*}"
-            #echo "$(clingo $enc $ins -q  --time-limit=$TIMEOUT)" >> $CSVFILE
             echo "$1,$enc,bb,$id,$(clingo $enc $ins -q  --time-limit=$TIMEOUT | sed ':a;N;$!ba;s/\n//g'| sed -E "s/.*(\.|p)(UNSATISFIABLE|SATISFIABLE|UNKNOWN|OPTIMUM FOUND)(TIME LIMIT   : [ 0-9]+)?(Models       : [0-9+]*)?(  Optimum    : [yesunknown]*)?(Optimization : )?([0-9 ]+)?Calls        : [0-9+]*Time         : ([0-9.]*)s \(Solving: ([0-9.]*)s ... Model: [0-9.]*s Unsat: [0-9.]*s\)CPU Time     : [0-9.]*s/\2,\8,\9,\7/")" >> $CSVFILE
         done               
     done 
